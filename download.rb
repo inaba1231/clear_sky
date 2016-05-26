4604ac85a9895620999fcf00b3f8e91350740bcd
tanboonjoon <tbj_999@hotmail.com>
2016-05-26 19:02:56 +0800

    download.rb

--- download.rb
+++ download.rb
@@ -1,8 +1,68 @@
+require 'rubygems'
+require 'nokogiri'
 require 'open-uri'
 
-path = "http://www.weather.gov.sg/files/rainarea/50km/v2/dpsri_70km_2016052318350000dBR.dpsri.png"
-open(path) {|f|
-   File.open("whatever_file.jpg","wb") do |file|
-     file.puts f.read
-   end
-}
+
+#retrieve time now  round off the minute to the nearest 5
+timeNow = Time.now
+
+#round off the minute to the nearest 5, create a new Time obj that exclude seconds
+check = timeNow.min % 10
+if check % 10 == 0
+	timeStamp = Time.new(timeNow.year,timeNow.month,timeNow.day, timeNow.hour,timeNow.min, 0, "+08:00")
+
+elsif check >= 5
+	minute = timeNow.min - (check - 5)
+	timeStamp = Time.new(timeNow.year,timeNow.month,timeNow.day, timeNow.hour, minute, 0, "+08:00")
+
+elsif check < 5
+	minute = timeNow.min - check 
+	timeStamp = Time.new(timeNow.year,timeNow.month,timeNow.day, timeNow.hour, minute, 0, "+08:00")
+end
+
+# store 19 5 minute interval time obj starting from 10 min ago in a arr
+timeArr = Array.new
+time = timeStamp - 600
+for i in 0...18
+  timeArr[i] = time
+  time-= 600
+end
+
+
+#loop through the array and save each timestamp image of the rain
+timeArr.each do |t|
+  
+  year = t.year
+  month = t.strftime("%m")
+  day = t.strftime("%d")
+  hour = t.strftime("%H")
+  min = t.strftime("%M")
+
+
+  image_path = "http://www.weather.gov.sg/files/rainarea/50km/v2/dpsri_70km_#{year}#{month}#{day}#{hour}#{min}0000dBR.dpsri.png"
+  image_name = image_path.split("v2/")[1]
+
+  open(image_path) {|f|
+    
+    File.open("#{image_name}","wb") do |file|
+      
+      file.puts f.read
+    end
+  }
+
+end
+
+
+
+# Using Nokogiri to extract the time,date
+=begin
+weather_link = 'http://www.weather.gov.sg/weather-rain-area-50km/'
+html = Nokogiri::HTML(open(weather_link))
+time_text = html.css('ul.list-unstyled').css('li').map { |e|  e['id']}.compact
+
+time_text.each do |t|
+puts t
+end
+/
+=end
+
