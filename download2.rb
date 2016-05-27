6e1277269b1baad27d507ae0f1f3b106b0106f14
tanboonjoon <tbj_999@hotmail.com>
2016-05-27 14:23:25 +0800

    download2 for endless loop, download 10min interval to 5min

--- download2.rb
+++ download2.rb
@@ -20,35 +20,48 @@ elsif check < 5
 	timeStamp = Time.new(timeNow.year,timeNow.month,timeNow.day, timeNow.hour, minute, 0, "+08:00")
 end
 
-# store 19 5 minute interval time obj starting from 10 min ago in a arr
-timeArr = Array.new
-time = timeStamp - 600
-for i in 0...19
-  timeArr[i] = time
-  time-= 300
-end
 
+#start from 10min ago
+timeStamp-= 600
 
 #loop through the array and save each timestamp image of the rain
-timeArr.each do |t|
-  
-  year = t.year
-  month = t.strftime("%m")
-  day = t.strftime("%d")
-  hour = t.strftime("%H")
-  min = t.strftime("%M")
+# boolean statement to ensure mining are looping
+mining = true
 
 
-  image_path = "http://www.weather.gov.sg/files/rainarea/50km/v2/dpsri_70km_#{year}#{month}#{day}#{hour}#{min}0000dBR.dpsri.png"
-  image_name = image_path.split("v2/")[1]
 
-  open(image_path) {|f|
+# mine backward and save each timestmap image of the rain
+# any exception will cause the program to sleep for 60 seconds before continuing
+
+#loop through the array and save each timestamp image of the rain
+while mining == true do
+
+  begin
+      year = timeStamp.year
+      month = timeStamp.strftime("%m")
+      day = timeStamp.strftime("%d")
+      hour = timeStamp.strftime("%H")
+      min = timeStamp.strftime("%M")
+
+      image_path = "http://www.weather.gov.sg/files/rainarea/50km/v2/dpsri_70km_#{year}#{month}#{day}#{hour}#{min}0000dBR.dpsri.png"
+      image_name = image_path.split("v2/")[1]
+
+      open(image_path) {|f|
+        
+        File.open("#{image_name}", "wb") do |file|
+          
+          file.puts f.read
+      end
+    }
+  rescue
+    #exception, sleep and retry again just incase exception is not caused
+    #by end of  tunnel/ invalid link
+    sleep(60)
+    retry
+  end
     
-    File.open("#{image_name}","wb") do |file|
-      
-      file.puts f.read
-    end
-  }
+  timeStamp-= 300
+  sleep(10)
 
 end
 
